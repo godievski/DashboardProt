674f4f7cd0d0562187449c4eaf8c35dfd49c4664
godievski <die.sux@hotmail.com>
2016-09-18 20:21:21 -0500

    something gud

--- js/scripts.js
+++ js/scripts.js
@@ -20,17 +20,27 @@ function init(){
 	}
 
 	var $menuLeft_button = $(".menu_left-button");
-	var $contentMain = $(".content-main");
+	var $wrapper = $(".wrapper");
 	var $menuLeft = $(".menu_left");
 	$menuLeft_button.on("click", displayMenu);
 
 	function displayMenu(){
 		if($menuLeft.css("left") != "0px"){
 			$menuLeft.css("left","0px");
-			$contentMain.css("padding-left","250px");
+			$wrapper.css("margin-left","250px");
 		} else{
 			$menuLeft.css("left","-250px");
-			$contentMain.css("padding-left","0px");
+			$wrapper.css("margin-left","0px");
 		}
 	}
+
+	/*CHARTS*/
+	var chartOne = new Morris.Donut({
+	  element: 'chartOne',
+	  data: [
+	    { label: 'In Use', value: 23.40 },
+	    { label: 'Free', value: 76.60 },
+	  ],
+	  formatter: function(x){return x+"%"}
+	});
 }
